d40a8988650b9b1affaabb68673d151d6beca046
JaBaZebs <arturdudka92@gmail.com>
2023-04-28 21:51:46 +0300

    top

---
+++ public/index.jsx
@@ -0,0 +1,11 @@
+import App from "../src/App.jsx"
+
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("app")
+  );
+root.render(
+    <App/>
+);
